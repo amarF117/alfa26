34f3547fe3c9efdd0a0749c22adc39f2b3b2980c
amarF117 <76045959+amarF117@users.noreply.github.com>
2021-03-07 12:55:10 +0100

    Add files via upload

---
+++ alfa26.py
@@ -0,0 +1,48 @@
+# Car centar test!
+user = input("Pick a model")
+
+bmw = {
+    " You have Pick" 
+    "Name" : "BMW M3" ,
+    "Year"  : "2000 ",
+    "Doors" : "3" ,
+    "Engine" : "3.2 L S54 I6",
+    "price" : "12.000 BAM",
+}
+
+if user == bmw: 
+    print(bmw)
+
+mercedes = {
+    "You have pick "
+    "Name" : "Mercedes AMG",
+    "year": 2000,
+    "Doors" : 4,
+    "Engine" : " Buick Fireball V6 ",
+    "price" : "9.000 BAM"
+}
+
+if user == mercedes:
+    print(mercedes)
+
+mazda = {
+    "You have pick"
+    "Name": "MAZDA Rx8",
+    "Year" : 2002,
+    "Door" : 3,
+    "Engine" : "1.3L RENESIS ",
+    "Price" : "15.000 BAM"
+}
+if user == mazda:
+    print(mazda)
+
+volvo = {
+    "You have pick"
+    "Name": "Volvo  V40 ",
+    "year": 2013,
+    "Doors": 4,
+    "Engine" : "1.6 L EcoBoost I4 ",
+    "prince" : "22.000 BAM "
+}
+if user == volvo:
+    print(volvo)
